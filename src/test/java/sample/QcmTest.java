2605e444dfaaa24a064e8a9e1d12b7c2634eabc1
Yufei6 <m.liuyufei@qq.com>
2019-03-15 19:02:50 +0100

    some tests

--- src/test/java/sample/QcmTest.java
+++ src/test/java/sample/QcmTest.java
@@ -1,5 +1,6 @@
 package sample;
 
+import javafx.scene.control.TreeItem;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -8,81 +9,120 @@ import org.xml.sax.SAXException;
 
 import java.io.IOException;
 
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
-
-import static org.junit.Assert.assertEquals;
 
 public class QcmTest {
     private SuperBank sb1;
 
     @Before
     public void setUp() throws javax.xml.parsers.ParserConfigurationException, SAXException, IOException, WrongQuestionTypeException {
-        sb1 = spy(new SuperBank());
+        sb1 = new SuperBank();
+        sb1.generateTreeWithQuestion();
+        sb1.extractId_Path();
     }
 
 
+
     @Test
-    public void InitWithoutParamaterOK1() {
-        Qcm b1 = new Qcm();
-        assertEquals(b1.getName(), "Qcmdefaut0");
-        Qcm b2 = new Qcm();
-        assertEquals(b2.getName(), "Qcmdefaut1");
-        Qcm b3 = new Qcm();
-        assertEquals(b3.getName(), "Qcmdefaut2");
+    public void InitOK1(){
+        Qcm qcm1 = new Qcm("./target/Qcm/qcm_test_001","qcm_test_001", sb1);
+        assertEquals(qcm1.getName(),"qcm_test_001");
     }
 
 
-    @Test
-    public void InitWithParameterOK1() {
-        when(sb1.find("100")).thenReturn("./target/Question/100.xml");
-        when(sb1.find("200")).thenReturn("./target/Question/200.xml");
-        when(sb1.find("300")).thenReturn("./target/Question/200.xml");
-        Qcm b1 = new Qcm("./target/Qcm/Qcm_test_001.xml", sb1);
-        assertEquals(b1.getName(), "Qcm_test_001");
-    }
 
 
     @Test
-    public void saveOK1() throws WrongQuestionTypeException {
-        when(sb1.find("100")).thenReturn("./target/Question/100.xml");
-        when(sb1.find("200")).thenReturn("./target/Question/200.xml");
-        when(sb1.find("300")).thenReturn("./target/Question/300.xml");
-        Qcm b1 = new Qcm("./target/Qcm/Qcm_test_001.xml", sb1);
-        b1.save();
-        Qcm b2 = new Qcm("./target/Qcm/Qcm_test_001.xml", sb1);
-        assertEquals("Qcm_test_001", b2.getName());
-    }
-
-
-    @Test
-    public void addAndDeleteQuestionOK1() throws WrongQuestionTypeException {
-        when(sb1.find("100")).thenReturn("./target/Question/100.xml");
-        when(sb1.find("200")).thenReturn("./target/Question/200.xml");
-        when(sb1.find("300")).thenReturn("./target/Question/300.xml");
-        Qcm b1 = new Qcm("./target/Qcm/Qcm_test_001.xml", sb1);
-        Question q1 = new Question("./target/Question/300.xml");
-        b1.addQuestion(q1);
-        b1.save();
-        assertEquals(3, b1.getQuestionQuantite());
-        b1.deleteQuestion(q1);
-        b1.save();
-        assertEquals(2, b1.getQuestionQuantite());
-    }
-
-
-    @Test
-    public void exportOK1() {
-        when(sb1.find("100")).thenReturn("./target/Question/100.xml");
-        when(sb1.find("200")).thenReturn("./target/Question/200.xml");
-        when(sb1.find("300")).thenReturn("./target/Question/300.xml");
-        Qcm b1 = new Qcm("./target/Qcm/Qcm_test_001.xml", sb1);
+    public void AddAndDeleteQuestion(){
+        Qcm qcm1 = new Qcm("./target/Qcm/qcm_test_001","qcm_test_001", sb1);
+        assertEquals(qcm1.getName(),"qcm_test_001");
+        try {
+            Question q1 = sb1.findQuestion("3");
+            assertFalse(qcm1.hasThisNameQuestion(q1.getName()));
+            qcm1.addQuestion(q1);
+            assertTrue(qcm1.hasThisNameQuestion(q1.getName()));
+            qcm1.deleteQuestion(q1);
+            assertFalse(qcm1.hasThisNameQuestion(q1.getName()));
+        }catch(WrongQuestionTypeException e){
+            e.printStackTrace();
+        }
 
     }
 
     @Test
-    public void importOK1(){
-
+    public void ImportUneBanque(){
+        Qcm qcm1 = Qcm.Import("./target/Qcm/qcm_test_001.xml",sb1);
+        assertEquals(qcm1.getName(),"qcm_test_001");
     }
+
+    @Test
+    public void ExporttUneBanqueOK(){
+        Qcm qcm1 = new Qcm("./target/Qcm/qcm_test_001","qcm_test_001", sb1);
+        assertEquals(qcm1.getName(),"qcm_test_001");
+        assertTrue(qcm1.Export("./target/qcm_Export/", "qcm_test_001_export").size()==0);
+        Qcm qcm2 = new Qcm("./target/Qcm_Export/qcm_test_001_export.xml", "qcm_test_001_export",sb1);
+        assertEquals(qcm2.getName(),"qcm_test_001_export");
+    }
+
+    @Test
+    public void ExporttUneBanqueKO(){
+        Qcm qcm1 = new Qcm("./target/Qcm/qcm_test_001","qcm_test_001", sb1);
+        assertEquals(qcm1.getName(),"qcm_test_001");
+        try {
+            qcm1.addQuestion(sb1.findQuestion("2"));
+            assertTrue(qcm1.Export("./target/Qcm_Export", "qcm_test_001_export1").size()==0);
+            qcm1.addQuestion(sb1.findQuestion("50"));
+            qcm1.deleteQuestion(sb1.findQuestion("2"));
+            qcm1.deleteQuestion(sb1.findQuestion("50"));
+        }catch(WrongQuestionTypeException e){
+            e.printStackTrace();
+        }
+    }
+
+
+    @Test
+    public void CreerQuestionTreeAndGetQuestionQuantite(){
+        Qcm qcm1 = new Qcm("./target/Qcm/bank_test_001","qcm_test_001", sb1);
+        assertEquals(qcm1.getName(),"qcm_test_001");
+        TreeItemWithQcmAndBank<String> root = new TreeItemWithQcmAndBank<>();
+        try {
+            Question q1 = sb1.findQuestion("3");
+            assertFalse(qcm1.hasThisNameQuestion(q1.getName()));
+            assertEquals(qcm1.getQuestionQuantite(),0);
+            qcm1.addQuestion(q1);
+            assertTrue(qcm1.hasThisNameQuestion(q1.getName()));
+            root = qcm1.createQuestionTree(root);
+            assertEquals(root.getChildren().size(),1);
+            assertEquals(qcm1.getQuestionQuantite(),1);
+            qcm1.deleteQuestion(q1);
+            assertFalse(qcm1.hasThisNameQuestion(q1.getName()));
+            root = qcm1.createQuestionTree(root);
+            assertEquals(root.getChildren().size(),1);
+            assertEquals(qcm1.getQuestionQuantite(),0);
+        }catch(WrongQuestionTypeException e){
+            e.printStackTrace();
+        }
+    }
+
+
+    @Test
+    public void ChangeNameAndChangePath(){
+        Qcm qcm1 = new Qcm("./target/Qcm/qcm_test_001","qcm_test_001", sb1);
+        assertEquals(qcm1.getName(),"qcm_test_001");
+        qcm1.changeName("new_name");
+        assertEquals(qcm1.getName(),"new_name");
+        qcm1.changeName("qcm_test_001");
+        assertEquals(qcm1.getName(),"qcm_test_001");
+        assertEquals(qcm1.getPath(),"./target/Qcm/qcm_test_001");
+        qcm1.changeName("new_path");
+        assertEquals(qcm1.getName(),"new_path");
+        qcm1.changeName("/target/Qcm/qcm_test_001.xml");
+        assertEquals(qcm1.getName(),"/target/Qcm/qcm_test_001.xml");
+    }
+
+
+
 
 }
